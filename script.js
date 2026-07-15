d8af7dece1786d1863161264be5fd479736cbe3b
sikyydnt <trypy200@gmail.com>
2026-07-15 21:54:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,112 +1,33 @@
-const lines = [
+const terminal = document.getElementById("terminal");
+
+const text = [
 "> DNT NETWORK BOOTING...",
-"> CONNECTING TO UNDERGROUND SERVER...",
-"> LOADING $IKY PROFILE...",
-"> CHECKING RELEASE DATABASE...",
-"> ACCESS GRANTED.",
+"> CONNECTING...",
+"> LOADING $IKY...",
+"> SYSTEM ONLINE.",
 "> NEXT DROP READY."
 ];
 
+let i = 0;
 
-let line = 0;
-let char = 0;
+function start(){
 
-const terminal = document.getElementById("terminal");
+if(i < text.length){
 
+terminal.innerHTML += text[i] + "<br>";
 
-function writeText(){
+i++;
 
-    if(line < lines.length){
-
-        if(char < lines[line].length){
-
-            terminal.innerHTML += lines[line].charAt(char);
-
-            char++;
-
-            setTimeout(writeText,60);
-
-        } else {
-
-            terminal.innerHTML += "<br>";
-
-            line++;
-            char = 0;
-
-            setTimeout(writeText,700);
-
-        }
-
-
-    } else {
-
-        showTimer();
-
-    }
+setTimeout(start,1000);
 
 }
 
+else{
 
-
-function showTimer(){
-
-    let timer = document.getElementById("timer");
-
-    if(timer){
-        timer.style.display = "block";
-    }
-
-    startCountdown();
+document.getElementById("timer").style.display="block";
 
 }
 
-
-
-function startCountdown(){
-
-    let releaseDate = new Date();
-
-    releaseDate.setDate(releaseDate.getDate()+7);
-
-
-    function update(){
-
-        let now = new Date();
-
-        let distance = releaseDate - now;
-
-
-        let days = Math.floor(distance/(1000*60*60*24));
-
-        let hours = Math.floor(
-        (distance%(1000*60*60*24))/(1000*60*60)
-        );
-
-        let minutes = Math.floor(
-        (distance%(1000*60*60))/(1000*60)
-        );
-
-        let seconds = Math.floor(
-        (distance%(1000*60))/1000
-        );
-
-
-        document.getElementById("countdown").innerHTML =
-        days+"d "+
-        hours+"h "+
-        minutes+"m "+
-        seconds+"s";
-
-
-    }
-
-
-    update();
-
-    setInterval(update,1000);
-
 }
 
-
-
-writeText();
+start();
